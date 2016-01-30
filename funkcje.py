0bd3a9bf81d6ed92a0d52aedb1aa4f25e380fb53
Piotr Latuszek <latuszek.piotr@pocztadzieci.pl>
2016-01-30 14:52:54 +0100

    obliczanie powierzni okien - ver.1

---
+++ funkcje.py
@@ -0,0 +1,21 @@
+# obliczam powierzchnie 3 okien
+
+szerokosc = 1.2 # [m]
+wysokosc = 1.2
+powierzchnia = szerokosc * wysokosc
+print powierzchnia
+
+szerokosc2 = 1.1 # [m]
+wysokosc2 = 1.3
+powierzchnia2 = szerokosc2 * wysokosc2
+
+print powierzchnia2
+
+szerokosc3 = 1.5 # [m]
+wysokosc3 = 1.3
+powierzchnia3 = szerokosc3 * wysokosc3
+
+print powierzchnia3
+
+suma_powierzni = powierzchnia + powierzchnia2 + powierzchnia3
+print "suma_powierzni = %s" % suma_powierzni
