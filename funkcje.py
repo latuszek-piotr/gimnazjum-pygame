585a62ba49f8ae67387600623fc8d810d5bd58a7
Piotr Latuszek <latuszek.piotr@pocztadzieci.pl>
2016-01-30 15:29:30 +0100

    obliczanie powierzni okien - ver.4: uzycie petli

--- funkcje.py
+++ funkcje.py
@@ -15,6 +15,5 @@ print "suma_powierzni = %s" % suma_powierzni
 # -------------------------------------------------------------------------
 suma_powierzni = 0
 for (szerokosc, wysokosc) in [(1.2, 1.2), (1.1, 1.3), (1.5, 1.3)]:
-    # print (szerokosc, wysokosc)
     suma_powierzni = suma_powierzni + oblicz_powierznie_prostokata(szerokosc, wysokosc)
 print "suma_powierzni = %s" % suma_powierzni
