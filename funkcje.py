0a566074cd86ad863183c35952947c07530c8848
Piotr Latuszek <latuszek.piotr@pocztadzieci.pl>
2016-01-30 15:15:05 +0100

    obliczanie powierzni okien - ver.3: 3 rozne sposoby

--- funkcje.py
+++ funkcje.py
@@ -3,7 +3,7 @@
 def oblicz_powierznie_prostokata(szerokosc, wysokosc):
     powierzchnia = szerokosc * wysokosc
     return powierzchnia
-
+# -------------------------------------------------------------------------
 
 powierzchnia = oblicz_powierznie_prostokata(1.2, 1.2)
 print powierzchnia
@@ -15,4 +15,15 @@ powierzchnia3 = oblicz_powierznie_prostokata(1.5, 1.3)
 print powierzchnia3
 
 suma_powierzni = powierzchnia + powierzchnia2 + powierzchnia3
+print "suma_powierzni = %s" % suma_powierzni
+
+# -------------------------------------------------------------------------
+suma_powierzni = oblicz_powierznie_prostokata(1.2, 1.2) + oblicz_powierznie_prostokata(1.1, 1.3) + oblicz_powierznie_prostokata(1.5, 1.3)
+print "suma_powierzni = %s" % suma_powierzni
+
+# -------------------------------------------------------------------------
+suma_powierzni = 0
+suma_powierzni = suma_powierzni + oblicz_powierznie_prostokata(1.2, 1.2)
+suma_powierzni = suma_powierzni + oblicz_powierznie_prostokata(1.1, 1.3)
+suma_powierzni = suma_powierzni + oblicz_powierznie_prostokata(1.5, 1.3)
 print "suma_powierzni = %s" % suma_powierzni
