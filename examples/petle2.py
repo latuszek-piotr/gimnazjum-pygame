553b0bb081308399fc5687115e7577e7f4b0dc7b
Piotr Latuszek <latuszek.piotr@pocztadzieci.pl>
2016-04-02 18:51:13 +0200

    przyklad petli po slowniku

---
+++ examples/petle2.py
@@ -0,0 +1,8 @@
+promienie_odwarznikuw = {"2kg": 5, "10kg": 10, "1kg": 6,"20kg": 20}
+for masa in promienie_odwarznikuw:
+
+    promien = promienie_odwarznikuw[masa]
+    print promien
+    powierzchnia = promien * promien * 3.14
+    print powierzchnia
+    powierzchnia
