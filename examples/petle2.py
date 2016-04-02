1a0a0d9d97b1c1e1ece060a6765e9649061898df
Piotr Latuszek <latuszek.piotr@pocztadzieci.pl>
2016-04-02 18:57:21 +0200

    przyklad petli po slowniku i skladania napisu przez %s

--- examples/petle2.py
+++ examples/petle2.py
@@ -1,8 +1,5 @@
 promienie_odwarznikuw = {"2kg": 5, "10kg": 10, "1kg": 6,"20kg": 20}
 for masa in promienie_odwarznikuw:
-
     promien = promienie_odwarznikuw[masa]
-    print promien
     powierzchnia = promien * promien * 3.14
-    print powierzchnia
-    powierzchnia
+    print "powierzchnia odwaznika o masie %s i promieniu %s cm = %s cm2" % (masa, promien, powierzchnia)
