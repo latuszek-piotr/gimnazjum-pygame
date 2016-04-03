e6e294b977045f23ad77d76568c52af26fb953c8
Wiktor Powroznik <wiktor220201@wp.pl>
2016-04-03 12:26:08 +0200

    muzyka menu glownego i okno menu i zdjecie jpg lanckorony 2

--- Okno gry .py
+++ Okno gry .py
@@ -5,9 +5,7 @@ import time
 # utworzenie okna
 window = pygame.display.set_mode((1000, 660))
 # ustawiamy tytul okna
-pygame.display.set_caption('Niebywale trudny')
+pygame.display.set_caption('Niebywale trudny rpg')
 # pobieramy informacje o ekranie - tle
 screen = pygame.display.get_surface()
 pygame.display.flip()
-input(pygame.event.get)()
-    grafika_lanckorony_menu = pygame.load.image(lanckorona_menu_glowne.jpg)()
