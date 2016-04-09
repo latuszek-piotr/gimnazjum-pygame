85e3614910e8fd4e9877ac87f2348e9f99e8492f
Wiktor Powroznik <wiktor220201@wp.pl>
2016-04-09 09:25:48 +0200

    menu cdn

--- Okno gry .py
+++ Okno gry .py
@@ -5,7 +5,11 @@ import time
 # utworzenie okna
 window = pygame.display.set_mode((1000, 660))
 # ustawiamy tytul okna
-pygame.display.set_caption('Niebywale trudny rpg')
+pygame.display.set_caption('Heroes of Village')
 # pobieramy informacje o ekranie - tle
-screen = pygame.display.get_surface()
+pygame.display.get_surface()
+screen.blit('grafika/lanckorona_menu_glowne.jpg', (500,500)
+pygame.image.load('grafika/lanckorona_menu_glowne.jpg')
 pygame.display.flip()
+while True:
+    input(pygame.event.get())
