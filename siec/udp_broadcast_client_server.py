3d3e01e7867d993dcaa87feeee0f94d1e152a9c1
Dawid Pulka <yogi298@interia.pl>
2016-10-08 18:12:45 +0200

    broadcast server jako class

--- siec/udp_broadcast_client_server.py
+++ siec/udp_broadcast_client_server.py
@@ -2,12 +2,9 @@ import socket
 import sys
 import time
 import subprocess
-#import ipaddress
 import platform
 import re
 
-MAX = 65535
-PORT = 50004
 
 def get_host_ip():
     s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,12 +14,14 @@ def get_host_ip():
     s1.close()
     return ip_string
 
+
 def get_mask(ip_string):
     if platform.system() == 'Windows':
         return get_mask_from_ipconfig(ip_string)
     if platform.system() == 'Linux':
         return get_broadcast_ip_from_ip_addr(ip_string)
     return "255.255.255.0"
+
 
 def get_broadcast_ip_from_ip_addr(ip_string):
     proc = subprocess.Popen(['ip', 'addr'], stdout=subprocess.PIPE)
@@ -39,6 +38,7 @@ def get_broadcast_ip_from_ip_addr(ip_string):
                 return broadcast_addr
     return "255.255.255.0"
 
+
 def get_mask_from_ipconfig(ip_string):
     proc = subprocess.Popen('ipconfig', stdout=subprocess.PIPE)
     while True:
@@ -54,6 +54,7 @@ def get_mask_from_ipconfig(ip_string):
     # print 'mask = %s' % mask
     return mask
 
+
 def get_broadcast_ip(ip_string):
     if platform.system() == 'Windows':
         return get_broadcast_ip_from_ifconfig(ip_string)
@@ -63,9 +64,10 @@ def get_broadcast_ip(ip_string):
     parts[-1]='255'
     return ".".join(parts)
 
+
 def get_broadcast_ip_from_ifconfig(ip_string):
     """Broadcast IP is IP bitwise ORed with Bit-negated mask"""
-    mask_string = get_mask_from_ipconfig(host_ip)
+    mask_string = get_mask_from_ipconfig(ip_string)
     mask_int_parts = [(~int(part) & 0xFF) for part in mask_string.split('.')]
     # print "inverted_mask %s" % mask_int_parts
     ip_int_parts = [(int(part) & 0xFF) for part in ip_string.split('.')]
@@ -77,39 +79,71 @@ def get_broadcast_ip_from_ifconfig(ip_string):
     # print broadcast_ip
     return broadcast_ip
 
-host_ip = get_host_ip()
-# ip_mask = get_mask(host_ip)
-# h_ip = ipaddress.IPv4Interface(socket.inet_aton(host_ip))
-# print h_ip
-# ip_mask = h_ip.network.netmask
-broadcast_ip = get_broadcast_ip(host_ip)
-# broadcast_ip = str(h_ip.network.broadcast_address)
 
-print "HOST IP      = %s" % host_ip
-# print "IP MASK      = %s" % ip_mask
-print "BROADCAST IP = %s" % broadcast_ip
+class NetworkConnection(object):
+    def __init__(self, port=50004, broadcast_ip=None, use_server=True):
+        self.port = port
+        self.max_buffsize = 65535
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.connection.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.connection.settimeout(0.1)
+        self.last_data = ''
+        self.last_sender_address = None
+        self.broadcast_ip = broadcast_ip
+        if use_server:
+            self.start_server()
+        self.start_client()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    def start_server(self):
+        self.connection.bind(('', self.port))
+        print 'Oczekuje na pozycje graczy pod adresem: {}'.format(self.connection.getsockname())
 
-if len(sys.argv) == 2 and sys.argv[1] == 'server':
-    s.bind(('', PORT))
-    print 'Listening for broadcasts at', s.getsockname()
-    while True:
-        data, address = s.recvfrom(MAX)
-        print 'The client at %r says: %r' % (address, data)
-        s.sendto('Hi, here is server! Answer to: {} time is: {}'.format(str(address), repr(time.time())), address)
+    def start_client(self):
+        if self.broadcast_ip is None:
+            host_ip = get_host_ip()
+            self.broadcast_ip = get_broadcast_ip(host_ip)
+        self.broadcast_address = (self.broadcast_ip, self.port)
+        print 'Rozglaszam swoja pozycje gracza na adres: {}'.format(str(self.broadcast_address))
 
-elif len(sys.argv) == 2 and sys.argv[1] == 'client':
-    s.settimeout(0.1)
-    s.sendto('Broadcast message ---! time is: {}'.format(repr(time.time())), (broadcast_ip, PORT))
-    try:
-        data, address = s.recvfrom(MAX)
-        print 'The server at %r says: %r' % (address, data)
-    except socket.timeout:
-        pass
-    try:
-        data, address = s.recvfrom(MAX)
-        print 'The server at %r says: %r' % (address, data)
-    except socket.timeout:
-        pass
+    def shutdown(self):
+        self.connection.close()
+
+    def receive(self):
+        try:
+            self.last_data, self.last_sender_address = self.connection.recvfrom(self.max_buffsize)
+            print 'Dostalem "%s" od: %s' % (self.last_data, self.last_sender_address)
+            return self.last_data
+        except socket.timeout:
+            return None
+
+    def send_to_last_sender(self, data):
+        if self.last_sender_address is not None:
+            print 'Sending "%s" to: %s' % (data, self.last_sender_address)
+            self.connection.sendto(data, self.last_sender_address)
+
+    def broadcast(self, data):
+        print 'Sending "%s" to: %s' % (data, self.broadcast_address)
+        self.connection.sendto(data, self.broadcast_address)
+
+if __name__ == '__main__':
+    broadcast_ip = None
+    if len(sys.argv) == 3:
+        broadcast_ip = sys.argv[2]
+
+    if sys.argv[1] == 'client':
+        net_connection = NetworkConnection(broadcast_ip=broadcast_ip, use_server=False)
+    else:
+        net_connection = NetworkConnection(broadcast_ip=broadcast_ip)
+
+    if len(sys.argv) == 2 and sys.argv[1] == 'server':
+        while True:
+            data = net_connection.receive()
+            if data:
+                answer = 'Hi, here is server! My time is: {}'.format(repr(time.time()))
+                net_connection.send_to_last_sender(data=answer)
+
+    elif len(sys.argv) >= 2 and sys.argv[1] == 'client':
+        msg = 'Broadcast message ---! time is: {}'.format(repr(time.time()))
+        net_connection.broadcast(data=msg)
+        net_connection.receive()
+        net_connection.receive()
