78e59555ffd0217ee9a18623a975bb50d13705af
Wiktor Powroznik <wiktor220201@wp.pl>
2016-10-14 19:43:28 +0200

    obrazek z kwiatem

--- heroesofpygame/flower.py
+++ heroesofpygame/flower.py
@@ -2,7 +2,7 @@ import pygame
 import os
 
 class Flower(object):
-    flower_img =  os.path.join('grafika', 'wiktor_wesoly.png')
+    flower_img =  os.path.join('grafika', 'flower.png')
 
     def __init__(self, pos=(410, 300), size=50):
         self.img = pygame.transform.scale(pygame.image.load(Flower.flower_img).convert_alpha(), (size, size+10))
