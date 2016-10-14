7e55c1fd1a1f153b72b9326ce14dfcec1a3bd9a8
Wiktor Powroznik <wiktor220201@wp.pl>
2016-10-14 20:18:10 +0200

    wprowadzenie losowosci - jaki kwiat wyswietlimy

--- heroesofpygame/flower.py
+++ heroesofpygame/flower.py
@@ -1,11 +1,21 @@
 import pygame
 import os
+import random
+
 
 class Flower(object):
-    flower_img =  os.path.join('grafika', 'flower.png')
+    flowers_img = [  os.path.join('grafika', 'flower.png'),
+                     os.path.join('grafika', 'kwiot.jpg'),
+                     os.path.join('grafika', 'flower1.png'),
+                     os.path.join('grafika', 'flower2.png'),
+                     os.path.join('grafika', 'flower3.png'),
+                     os.path.join('grafika', 'flower4.png'),
+                     os.path.join('grafika', 'kwiot.jpg')]
+
 
     def __init__(self, pos=(410, 300), size=50):
-        self.img = pygame.transform.scale(pygame.image.load(Flower.flower_img).convert_alpha(), (size, size+10))
+        selected_img =random.choice(Flower.flowers_img)
+        self.img = pygame.transform.scale(pygame.image.load(selected_img).convert_alpha(), (size, size+10))
         self.pos = pos
 
     def draw(self, screen):
