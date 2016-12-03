d58b9d40f21a5542526b629d1a40a133e014384e
Dawid Pulka <yogi298@interia.pl>
2016-12-03 19:47:02 +0100

    obrazy z minami jako slownik w klasie Piotr

--- heroesofpygame/piotr.py
+++ heroesofpygame/piotr.py
@@ -8,13 +8,18 @@ class Piotr(Player):
     sad_img = os.path.join('grafika', 'piotr_smutny.png')
     angry_img = os.path.join('grafika', 'piotr_zly.png')
     surprised_img = os.path.join('grafika', 'piotr_zdziwiony.png')
-    scared_img = os.path.join('grafika', 'piotr_przestraszony.png')
+    scared_img = os.path.join('grafika', 'piotr_zdziwiony.png')  # TODO dorobic 'piotr_przestraszony.png')
 
     def __init__(self, pos=(350, 50), size=50):
         super(Piotr, self).__init__(pos, size)
         self.mood = 'happy'
-        self.img = pygame.transform.scale(pygame.image.load(Piotr.happy_img).convert_alpha(), (size, size+10))
+        self.images = {'happy': pygame.transform.scale(pygame.image.load(Piotr.happy_img).convert_alpha(), (size, size+10)),
+                       'sad': pygame.transform.scale(pygame.image.load(Piotr.sad_img).convert_alpha(), (size, size+10)),
+                       'angry': pygame.transform.scale(pygame.image.load(Piotr.angry_img).convert_alpha(), (size, size+10)),
+                       'suprised': pygame.transform.scale(pygame.image.load(Piotr.surprised_img).convert_alpha(), (size, size+10)),
+                       'scared': pygame.transform.scale(pygame.image.load(Piotr.scared_img).convert_alpha(), (size, size+10))}
 
     def draw(self, screen):
         # Copy image to screen:
-        screen.blit(self.img, [self.rect.x, self.rect.y])
+        img = self.images[self.mood]
+        screen.blit(img, [self.rect.x, self.rect.y])
