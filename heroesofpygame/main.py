3646fc43d5670dad875e30e96a740b8ad2c02b0f
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2016-12-31 19:11:30 +0100

    wprowadzenie klasy dla stanu rozgrywka

--- heroesofpygame/main.py
+++ heroesofpygame/main.py
@@ -2,19 +2,15 @@ import os
 import sys
 from pygame.locals import *
 import pygame
+
+from heroesofpygame.rozpoczecie import Rozpoczecie
+from heroesofpygame.rozgrywka import Rozgrywka
 from heroesofpygame.przegrana import Przegrana
 from heroesofpygame.wygrana import Wygrana
-from heroesofpygame.rozpoczecie import Rozpoczecie
 from heroesofpygame.zakonczenie import Zakonczenie
+
 from heroesofpygame import Pietro
-from heroesofpygame.player import Player
-from heroesofpygame.wiktor import Wiktor
-from heroesofpygame.dominik import Dominik
-from heroesofpygame.piotr import Piotr
-from heroesofpygame.dawid import Dawid
-from heroesofpygame.parter import Parter
-from heroesofpygame.strzal import Strzal
-from heroesofpygame.udp_broadcast_client_server import NetworkConnection
+
 from heroesofpygame.szarancza import Szarancza
 
 clock = pygame.time.Clock()
@@ -28,53 +24,26 @@ pygame.display.set_caption("szkola_1_pietro")
 srodek_ekranu = (650, 325)
 
 ########################### zmienne globalne
+active_player_name = sys.argv[1]
 
 szerokosc_ekranu = 1300
 wysokosc_ekranu = 650
 screen = pygame.display.set_mode((szerokosc_ekranu, wysokosc_ekranu))
-players = {  # lista mozliwych graczy
-    "Wiktor":  Wiktor(),
-    "Dominik":  Dominik(),
-    "Dawid":  Dawid(),
-    "Piotr":  Piotr(),
-    }
-remote_players = {}  # lista graczy zdalnych
-active_player_name = sys.argv[1]
-active_player = players[active_player_name]  # aktywny gracz
-player1 = players["Wiktor"]
-player2 = players["Dominik"]
-player3 = players["Dawid"]
-player4 = players["Piotr"]
-parter = Parter()
-
-aktywna_sala = parter.klasa_info  # wyswietlana sala na ktorej dzieje sie akcja
-aktywna_sala.przeskaluj(1300, 650)
-aktywna_sala.dodaj_kwiat()
-
-lewy_dol = aktywna_sala.daj_naroznik(ktory='lewy-dolny')
-pozycja_startowa = (lewy_dol[0]+10, lewy_dol[1] - 60)
-aktywna_szarancza = None
-
-strzal = Strzal()
-sound = pygame.mixer.Sound('dzwiek/fanfary.wav')
-all_objects = None  # wszystkie obiekty ktore moga wchodzic w kolizje
 
 stan_gry = "rozpoczecie"
-# stan_gry = "rozgrywka"  #mozliwe : "rozpoczecie", "rozgrywka", "przegrana", "wygrana", "zakonczenie"
+stan_gry = "rozgrywka"  #mozliwe : "rozpoczecie", "rozgrywka", "przegrana", "wygrana", "zakonczenie"
 # stan_gry = "przegrana"
 # stan_gry = "wygrana"
 # stan_gry = "zakonczenie"
+
+rozpoczecie = Rozpoczecie(szerokosc_ekranu, wysokosc_ekranu)
+rozgrywka = Rozgrywka(szerokosc_ekranu, wysokosc_ekranu, active_player_name)
 przegrana = Przegrana(szerokosc_ekranu, wysokosc_ekranu)
 wygrana = Wygrana(szerokosc_ekranu, wysokosc_ekranu)
-rozpoczecie = Rozpoczecie(szerokosc_ekranu, wysokosc_ekranu)
 zakonczenie = Zakonczenie(szerokosc_ekranu, wysokosc_ekranu)
 
-######################################## inicjalizacja
 
-all_objects = aktywna_sala.walls()
-for player_name in players:
-    all_objects.append(players[player_name])
-all_objects.append(strzal)
+######################################## inicjalizacja
 
 
 def is_game_finished(event):
@@ -86,74 +55,10 @@ def is_game_finished(event):
     return running
 
 
-def handle_remote_player(net_connection, active_player, players, remote_players):
-    network_data = net_connection.receive()
-    # x=363, y=231, name=Wiktor
-
-    if network_data is not None:
-        (pos, name, action) = Player.unpack_network_record(network_data)
-        if name != active_player.__class__.__name__:
-            # print "network: %s, %s, %s" % (pos, name, action)
-            if action == 'move':
-                if name not in remote_players:
-                    joining_player = players[name]
-                    remote_players[name] = joining_player
-                moved_player = remote_players[name]
-                moved_player.move_to(pos)
-            elif action == 'join':
-                joining_player = players[name]
-                remote_players[name] = joining_player
-            elif action == 'leave':
-                leaving_player_name = name
-                del remote_players[leaving_player_name]
-
-
-def broadcast_active_player(active_player, net_connection, action='move', await_confirmation=False):
-    if net_connection != None:
-        network_record = active_player.serialize_for_network(action=action)
-        net_connection.broadcast(data=network_record, await_confirmation=await_confirmation)
-
-
-def move_player_using_keyboard(key_left, key_right, key_up, key_down, active_player, all_objects, net_connection):
-    key = pygame.key.get_pressed()
-    if key[key_left]:
-        active_player.move(-1, 0, all_objects)
-    elif key[key_right]:
-        active_player.move(1, 0, all_objects)
-    elif key[key_up]:
-        active_player.move(0, -1, all_objects)
-    elif key[key_down]:
-        active_player.move(0, 1, all_objects)
-    else:
-        return  # no move
-    broadcast_active_player(active_player, net_connection)
-
-
-def draw_remote(screen, remote_players):
-    for player in remote_players.values():
-        player.draw(screen)
-
-
-def sprawdz_strzal(strzal, x, y):
-    key = pygame.key.get_pressed()
-    if key[pygame.K_SPACE]:
-        strzal.ustaw_pozycje(x, y)
-        strzal.start()
-        return True
-    return False
-
-
-def muzyka_pod_przyciskiem():
-    key = pygame.key.get_pressed()
-    muza = pygame.mixer.Sound('dzwiek/dzwiek_walki/dzwiek_porazki.wav')
-    if key[pygame.K_b]:
-        muza.play()
-
-
 # ---------------------------- glowna petla zdarzen pygame
-net_connection = NetworkConnection(active_player.nazwa)
-active_player.move_to(srodek_ekranu)
-# broadcast_active_player(active_player, net_connection, action='join', await_confirmation=True)
+
+rozgrywka.active_player.move_to(srodek_ekranu)  # TODO do on_entry() stanu rozgrywka
+# rozgrywka.broadcast_active_player(active_player, rozgrywka.net_connection, action='join', await_confirmation=True)
 
 running = True
 screen.fill((0, 0, 0))
@@ -170,88 +75,46 @@ while running:
         ################################### Akcje na planszy gry zalezne od eventow pygame
         if stan_gry == "rozpoczecie":
             decyzja = rozpoczecie.grac_ponownie(event)
-            if decyzja is None:
-                pass  # nic nie robie, brak decyzji
-            elif decyzja == "TAK":
+            if decyzja == "TAK":
                 stan_gry = "rozgrywka"
-
-        elif stan_gry == "rozgrywka":  #mozliwe : "rozpoczecie", "rozgrywka", "przegrana", "wygrana", "zakonczenie"
-            handle_remote_player(net_connection, active_player, players, remote_players)
-
-            muzyka_pod_przyciskiem()
 
         elif stan_gry == "wygrana":
             decyzja = wygrana.grac_ponownie(event)
-            if decyzja is None:
-                pass  # nic nie robie, brak decyzji
-            elif decyzja == "TAK":
+            if decyzja == "TAK":
                 stan_gry = "rozgrywka"
-            else:
+            elif decyzja == "NIE":
                 stan_gry = "zakonczenie"
 
         elif stan_gry == "przegrana":
             decyzja = przegrana.grac_ponownie(event)
-            if decyzja is None:
-                pass  # nic nie robie, brak decyzji
-            elif decyzja == "TAK":
+            if decyzja == "TAK":
                 stan_gry = "rozgrywka"
-            else:
+            elif decyzja == "NIE":
                 stan_gry = "zakonczenie"
 
         elif stan_gry == "zakonczenie":
             decyzja = zakonczenie.grac_ponownie(event)
-            if decyzja is None:
-                pass  # nic nie robie, brak decyzji
-            elif decyzja == "NIE":
+            if decyzja == "NIE":
                 running = False
                 break
 
     ################################### Akcje na planszy gry niezalezne od eventow pygame
     if stan_gry == "rozgrywka":
-        if aktywna_szarancza is None:
-            aktywna_szarancza = Szarancza(pozycja_startowa)
-            aktywna_szarancza.start(aktywna_sala.daj_kwiat())
-        # handle_remote_player(net_connection, active_player, players, remote_players)
-
-        # Move the player if an arrow key is pressed
-        move_player_using_keyboard(pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, active_player, all_objects, net_connection)
-        move_player_using_keyboard(pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, player2, all_objects, None)
-        move_player_using_keyboard(pygame.K_j, pygame.K_l, pygame.K_i, pygame.K_k, player3, all_objects, None)
-        move_player_using_keyboard(pygame.K_f, pygame.K_h, pygame.K_t, pygame.K_g, player4, all_objects, None)
-
-        czy_strzela = sprawdz_strzal(strzal, x=active_player.rect.x, y=active_player.rect.y)
-        if czy_strzela:
-            active_player.zmien_humor("angry")
-
-        wynik = aktywna_szarancza.update_pozycji_i_kolizji(all_objects)
-        if wynik is None:
-            pass  # nic nie robie, nic sie nie stalo
-        elif wynik == "zjedzony_kwiat":
-            aktywna_szarancza = None
-            stan_gry = "przegrana"
-        elif wynik == "martwa_szarancza":
-            aktywna_szarancza = None
-            stan_gry = "wygrana"
+        stan_gry = rozgrywka.handle_clock_tick()
 
     ################################### Rysowanie planszy gry
 
     if stan_gry == "rozpoczecie":
         rozpoczecie.draw(screen)
-    elif stan_gry == "rozgrywka":  #mozliwe : "rozpoczecie", "rozgrywka", "przegrana", "wygrana", "zakonczenie"
+    elif stan_gry == "rozgrywka":
         screen.fill((0, 0, 0))
-        # parter.draw(screen)     # rysujemy go tylko w trybie "podglad mapy"
-        aktywna_sala.draw(screen)
         # flower_1.draw(screen)   # to ma sie narysowac w sali
         # flower_2.draw(screen)
-        active_player.draw(screen)
-        draw_remote(screen, remote_players)
         # player1.draw(screen)
         # player2.draw(screen)
         # player3.draw(screen)
         # player4.draw(screen)
-        if aktywna_szarancza and aktywna_szarancza.is_started():
-            aktywna_szarancza.draw(screen)
-        strzal.draw(screen)
+        rozgrywka.draw(screen)
     elif stan_gry == "wygrana":
         screen.fill((0, 0, 0))
         wygrana.draw(screen)
@@ -264,4 +127,4 @@ while running:
 
     pygame.display.flip()
 
-#broadcast_active_player(active_player, net_connection, action='leave')
+#rozgrywka.broadcast_active_player(rozgrywka.active_player, rozgrywka.net_connection, action='leave')
---
+++ heroesofpygame/rozgrywka.py
@@ -0,0 +1,167 @@
+import pygame
+import random
+
+from heroesofpygame.strzal import Strzal
+
+from heroesofpygame.player import Player
+from heroesofpygame.wiktor import Wiktor
+from heroesofpygame.dominik import Dominik
+from heroesofpygame.piotr import Piotr
+from heroesofpygame.dawid import Dawid
+
+from heroesofpygame.szarancza import Szarancza
+
+from heroesofpygame.parter import Parter
+
+from heroesofpygame.udp_broadcast_client_server import NetworkConnection
+
+class Rozgrywka(object):
+    def __init__(self, szerokosc, wysokosc, active_player_name):
+        self.szerokosc = szerokosc
+        self.wysokosc = wysokosc
+        self.strzal = Strzal()
+        self.players = {  # lista mozliwych graczy
+                        "Wiktor":  Wiktor(),
+                        "Dominik":  Dominik(),
+                        "Dawid":  Dawid(),
+                        "Piotr":  Piotr(),
+                       }
+        self.active_player = self.players[active_player_name]
+        self.net_connection = NetworkConnection(self.active_player.nazwa)
+        # self.player1 = self.players["Wiktor"]
+        # self.player2 = self.players["Dominik"]
+        # self.player3 = self.players["Dawid"]
+        # self.player4 = self.players["Piotr"]
+        self.remote_players = {}  # lista graczy zdalnych
+        self.parter = Parter()
+        self.aktywna_sala = self.wylosuj_sale()
+        self.aktywna_szarancza = None
+        self.all_objects = self.obiekty_mogace_wchodzic_w_kolizje()
+
+    def wylosuj_sale(self):
+        sala = self.parter.klasa_info  # wyswietlana sala na ktorej dzieje sie akcja #TODO losowanie; teraz na sztywno
+        return sala
+
+    def obiekty_mogace_wchodzic_w_kolizje(self):
+        all_objects = []
+        for player_name in self.players:
+            all_objects.append(self.players[player_name])
+        all_objects.append(self.strzal)
+        return all_objects
+
+    def sprawdz_strzal(self, x, y):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE]:
+            self.strzal.ustaw_pozycje(x, y)
+            self.strzal.start()
+            return True
+        return False
+
+    def move_player_using_keyboard(self, key_left, key_right, key_up, key_down, active_player, all_objects):
+        key = pygame.key.get_pressed()
+        if key[key_left]:
+            active_player.move(-1, 0, all_objects)
+        elif key[key_right]:
+            active_player.move(1, 0, all_objects)
+        elif key[key_up]:
+            active_player.move(0, -1, all_objects)
+        elif key[key_down]:
+            active_player.move(0, 1, all_objects)
+        else:
+            return  # no move
+        self.broadcast_active_player(active_player)
+
+    def broadcast_active_player(self, active_player, action='move', await_confirmation=False):
+        if self.net_connection != None:
+            network_record = active_player.serialize_for_network(action=action)
+            self.net_connection.broadcast(data=network_record, await_confirmation=await_confirmation)
+
+    def handle_remote_player(self):
+        network_data = self.net_connection.receive()
+        # x=363, y=231, name=Wiktor
+
+        if network_data is not None:
+            (pos, name, action) = Player.unpack_network_record(network_data)
+            if name != self.active_player.__class__.__name__:
+                # print "network: %s, %s, %s" % (pos, name, action)
+                if action == 'move':
+                    if name not in self.remote_players:
+                        joining_player = self.players[name]
+                        self.remote_players[name] = joining_player
+                    moved_player = self.remote_players[name]
+                    moved_player.move_to(pos)
+                elif action == 'join':
+                    joining_player = self.players[name]
+                    self.remote_players[name] = joining_player
+                elif action == 'leave':
+                    leaving_player_name = name
+                    del self.remote_players[leaving_player_name]
+
+    def draw_remote(self, screen, remote_players):
+        for player in remote_players.values():
+            player.draw(screen)
+
+    def wylosuj_pozycje_startowa_szaranczy(self):
+        lewy_dol = self.aktywna_sala.daj_naroznik(ktory='lewy-dolny')
+        przesuniecie = random.randint(10, self.aktywna_sala.room_width - 10)  #TODO dopracowac to losowanie
+        pozycja_startowa = (lewy_dol[0]+przesuniecie, lewy_dol[1] - 60)
+        return pozycja_startowa
+
+    def on_entry(self):
+        self.aktywna_sala.przeskaluj(self.szerokosc, self.wysokosc)
+        self.aktywna_sala.dodaj_kwiat()
+        pozycja_startowa = self.wylosuj_pozycje_startowa_szaranczy()
+        self.aktywna_szarancza = Szarancza(pozycja_startowa)
+        self.all_objects = self.obiekty_mogace_wchodzic_w_kolizje()
+        self.all_objects.extend(self.aktywna_sala.walls())
+
+        self.active_player.mood = 'happy'
+
+        self.aktywna_szarancza.start(self.aktywna_sala.daj_kwiat())
+
+    def handle_clock_tick(self):
+        if self.aktywna_szarancza is None:  # TODO: dac do on_entry() stanu rozgrywka
+            self.on_entry()
+
+        self.handle_remote_player()
+
+        # Move the player if an arrow key is pressed
+        self.move_player_using_keyboard(pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, self.active_player, self.all_objects)
+        # rozgrywka.move_player_using_keyboard(pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, player2, rozgrywka.all_objects)
+        # rozgrywka.move_player_using_keyboard(pygame.K_j, pygame.K_l, pygame.K_i, pygame.K_k, player3, rozgrywka.all_objects)
+        # rozgrywka.move_player_using_keyboard(pygame.K_f, pygame.K_h, pygame.K_t, pygame.K_g, player4, rozgrywka.all_objects)
+
+        czy_strzela = self.sprawdz_strzal(x=self.active_player.rect.x, y=self.active_player.rect.y)
+        if czy_strzela:
+            self.active_player.zmien_humor("angry")
+
+        wynik = self.aktywna_szarancza.update_pozycji_i_kolizji(self.all_objects)
+        if wynik is None:
+            pass  # nic nie robie, nic sie nie stalo
+        elif wynik == "zjedzony_kwiat":
+            self.aktywna_szarancza = None
+            porazka_sound = pygame.mixer.Sound('dzwiek/dzwiek_walki/dzwiek_porazki.wav') # TODO: dac do on_exit() stanu rozgrywka
+            porazka_sound.play()
+            return "przegrana"
+        elif wynik == "martwa_szarancza":
+            self.aktywna_szarancza = None
+            wygrana_sound = pygame.mixer.Sound('dzwiek/dzwiek_walki/dzwiek_sukcesu.wav')
+            wygrana_sound.play()
+            return "wygrana"
+
+        return "rozgrywka"
+
+    def draw(self, screen):
+        # # parter.draw(screen)     # rysujemy go tylko w trybie "podglad mapy"
+        self.aktywna_sala.draw(screen)
+        # # flower_1.draw(screen)   # to ma sie narysowac w sali
+        # # flower_2.draw(screen)
+        self.active_player.draw(screen)
+        self.draw_remote(screen, self.remote_players)
+        # # player1.draw(screen)
+        # # player2.draw(screen)
+        # # player3.draw(screen)
+        # # player4.draw(screen)
+        if self.aktywna_szarancza and self.aktywna_szarancza.is_started():
+            self.aktywna_szarancza.draw(screen)
+        self.strzal.draw(screen)
