bf8392a8699a51f5f61078f744c239138232bac5
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2017-01-02 22:43:44 +0100

    poprawki strzalu

--- heroesofpygame/strzal.py
+++ heroesofpygame/strzal.py
@@ -12,15 +12,15 @@ class Strzal(object):
     def __init__(self, pos=(910, 150), size=50):
         current_img = Strzal.strzal_img[0]
         self.images = [
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[0]).convert_alpha(), (50, 20)), (-20, 100)),
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[1]).convert_alpha(), (100, 100)),(0, 20)),
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[2]).convert_alpha(), (100, 120)),(20, 0)),
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[3]).convert_alpha(), (200, 120)),(40, 0)),
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[4]).convert_alpha(), (180, 120)),(80, 0)),
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[5]).convert_alpha(), (90, 120)),(110, 0)),
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[6]).convert_alpha(), (80, 120)),(110, 0)),
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[7]).convert_alpha(), (70, 120)),(110, 0)),
-                       (pygame.transform.scale(pygame.image.load(Strzal.strzal_img[8]).convert_alpha(), (60, 120)),(110, 0)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[0]).convert_alpha(), (50, 20)), True, False), (20, 100)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[1]).convert_alpha(), (100, 100)), True, False),(0, 20)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[2]).convert_alpha(), (100, 120)), True, False),(20, 0)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[3]).convert_alpha(), (200, 120)), True, False),(-30, 0)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[4]).convert_alpha(), (180, 120)), True, False),(50, 0)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[5]).convert_alpha(), (90, 120)), True, False),(180, 0)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[6]).convert_alpha(), (80, 120)), True, False),(190, 0)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[7]).convert_alpha(), (70, 120)), True, False),(200, 0)),
+                       (pygame.transform.flip(pygame.transform.scale(pygame.image.load(Strzal.strzal_img[8]).convert_alpha(), (60, 120)), True, False),(210, 0)),
                        ]
         self.dzwiek_strzalu = pygame.mixer.Sound('dzwiek/dzwiek_walki/strzal.wav')
         self.pos = list(pos)
@@ -54,11 +54,16 @@ class Strzal(object):
             else:
                 # Copy image to screen:
                 (image, (delta_x, delta_y)) = self.images[image_index]
-                pos = (self.pos[0] - delta_x, self.pos[1] + delta_y)
-                final_image, nowa_pozycja_obrazu = self.obroc_obraz(image, self.pos, pos)
+                pos = (self.pos[0] + delta_x, self.pos[1] + delta_y)
                 self.ustaw_rect_do_kolizji(pos[0], pos[1])
-                # screen.blit(image, pos)
-                screen.blit(final_image, nowa_pozycja_obrazu)
+                screen.blit(image, pos)
+                img_rect = image.get_rect().move(pos)
+                # pygame.draw.lines(screen, (0, 255, image_index*30), False, [img_rect.topleft, img_rect.bottomleft, img_rect.bottomright, img_rect.topright, img_rect.topleft], 1)
+
+                # final_image, nowa_pozycja_obrazu = self.obroc_obraz(image, self.pos, pos)
+                # img_rect = final_image.get_rect().move(nowa_pozycja_obrazu)
+                # pygame.draw.lines(screen, (0, 255, image_index*30), False, [img_rect.topleft, img_rect.bottomleft, img_rect.bottomright, img_rect.topright, img_rect.topleft], 1)
+                # screen.blit(final_image, nowa_pozycja_obrazu)
 
     def ktory_obraz(self):
         now = time.time()
