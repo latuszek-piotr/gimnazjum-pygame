2497a467b368464184c538f8a5f76e76b17d527a
Wiktor Powroznik <wiktor220201@wp.pl>
2017-01-05 20:20:26 +0100

    sasiedztwo sal w obiekcie drzwi

--- heroesofpygame/door.py
+++ heroesofpygame/door.py
@@ -10,4 +10,12 @@ class Door(Pixel):
         self.color = color
         self.rect = pygame.Rect(pos[0], pos[1], width, length)
 
+    def ustaw_miedzy_salami(self, sala_1, sala_2):
+        self.sala_1 = sala_1
+        self.sala_2 = sala_2
 
+    def daj_sale_sasiednia(self, sala):
+        if self.sala_1 == sala:
+            return self.sala_2
+        else:
+            return self.sala_1
