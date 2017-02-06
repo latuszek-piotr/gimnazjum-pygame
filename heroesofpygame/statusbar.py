72a06582b05984c9c040b64c4e49001f46b21f76
Piotr Latuszek <latuszek.piotr@pocztadzieci.pl>
2017-02-06 21:46:03 +0100

    szarancza macha skrzydłami na statusbar

--- heroesofpygame/statusbar.py
+++ heroesofpygame/statusbar.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import os
+import time
 from heroesofpygame.wall import NewWall
 from heroesofpygame.flower import Flower
 
@@ -34,6 +35,9 @@ def resetuj_wynik(ile_kwiatow, ile_szaranczy):
 
 class StatusBar(object):
     szarancza = os.path.join('grafika', 'szarancza', 'szarancza2_lot2.png')
+    lot1 = os.path.join('grafika', 'szarancza', 'szarancza2_lot1.png')
+    lot2 = szarancza
+    lot3 = os.path.join('grafika', 'szarancza', 'szarancza2_lot3.png')
     kwiat = os.path.join('grafika', 'status_flower.png')
 
     def __init__(self, pos, size=50, pionowy=True):
@@ -56,6 +60,11 @@ class StatusBar(object):
             szarancza_start_pos = (self.rect_lokalizacji.right+20, self.rect_lokalizacji.top+self.rect_lokalizacji.height/2)
         self.obszary_szaranczy = self.oblicz_obszary_statusu(szarancza_start_pos, rozmiar_obszaru, self.ile_szaranczy, pionowy)
         self.szarancza_img = pygame.transform.scale(pygame.image.load(StatusBar.szarancza).convert_alpha(), (int(rozmiar_obszaru*1.9*0.4), int(rozmiar_obszaru*0.4)))
+        self.szarancza_lot = [pygame.transform.scale(pygame.image.load(StatusBar.lot1).convert_alpha(), (int(rozmiar_obszaru*1.9*0.4), int(rozmiar_obszaru*0.4))),
+                              pygame.transform.scale(pygame.image.load(StatusBar.lot2).convert_alpha(), (int(rozmiar_obszaru*1.9*0.4), int(rozmiar_obszaru*0.4))),
+                              pygame.transform.scale(pygame.image.load(StatusBar.lot3).convert_alpha(), (int(rozmiar_obszaru*1.9*0.4), int(rozmiar_obszaru*0.4)))]
+        self.czas_machniecia_skrzydel = 0.1
+        self.start_time = time.time()
         self.kwiat_img = pygame.transform.scale(pygame.image.load(StatusBar.kwiat).convert_alpha(), (int(rozmiar_obszaru*0.8), int(rozmiar_obszaru*0.8)))
 
     def oblicz_obszary_statusu(self, start_pos, wielkosc_obszaru, ile_obszarow, pionowy=True):
@@ -89,9 +98,7 @@ class StatusBar(object):
             pozycja_napisu = (self.rect_lokalizacji.centerx - width/2, self.rect_lokalizacji.centery - height/2)
             screen.blit(text, pozycja_napisu)
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, (80,80,80), self.rect)
-        self.draw_lokalizacja(screen)
+    def draw_kwiaty(self, screen):
         # self.draw_obszary(screen, self.obszary_kwiatow)
         for idx, rect in enumerate(self.obszary_kwiatow):
             dx = (rect.width - self.kwiat_img.get_width()) / 2
@@ -103,13 +110,32 @@ class StatusBar(object):
                 pygame.draw.lines(screen, (255,0,0), False, [img_rect.topleft, img_rect.bottomright], 4)
                 pygame.draw.lines(screen, (255,0,0), False, [img_rect.bottomleft, img_rect.topright], 4)
 
+    def ktory_obraz(self):
+        now = time.time()
+        time_delta = now - self.start_time
+        ile_machniec = int(time_delta / self.czas_machniecia_skrzydel)
+        ktora_pozycja_lotu = ile_machniec % 3
+        return ktora_pozycja_lotu
+
+    def draw_szarancza(self, screen):
         # self.draw_obszary(screen, self.obszary_szaranczy)
         for idx, rect in enumerate(self.obszary_szaranczy):
             dx = (rect.width - self.szarancza_img.get_width()) / 2
             dy = (rect.height - self.szarancza_img.get_height()) / 2
             img_pos = (rect.left+dx, rect.top+dy)
-            screen.blit(self.szarancza_img, img_pos)
             if idx < daj_status().zabite_szarancze:
+                screen.blit(self.szarancza_img, img_pos)
                 img_rect = self.szarancza_img.get_rect().move(img_pos)
                 pygame.draw.lines(screen, (255,0,0), False, [img_rect.topleft, img_rect.bottomright], 4)
                 pygame.draw.lines(screen, (255,0,0), False, [img_rect.bottomleft, img_rect.topright], 4)
+            else:
+                image_index = self.ktory_obraz()
+                image_index = (image_index + idx) % 3
+                image = self.szarancza_lot[image_index]
+                screen.blit(image, img_pos)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (80,80,80), self.rect)
+        self.draw_lokalizacja(screen)
+        self.draw_kwiaty(screen)
+        self.draw_szarancza(screen)
